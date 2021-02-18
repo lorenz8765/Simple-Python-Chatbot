29c9e99c9aeac5f3d3c495580e8719fbaf94cc4f
adxvl <79146737+adxvl@users.noreply.github.com>
2021-02-18 13:40:04 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,109 +1,125 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "id": "computational-liabilities",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import tkinter\n",
-    "from tkinter import *\n",
-    "\n",
-    "\n",
-    "def send():\n",
-    "    msg = EntryBox.get(\"1.0\",'end-1c').strip()\n",
-    "    EntryBox.delete(\"0.0\",END)\n",
-    "\n",
-    "    if msg != '':\n",
-    "        ChatLog.config(state=NORMAL)\n",
-    "        ChatLog.insert(END, \"You: \" + msg + '\\n\\n')\n",
-    "        ChatLog.config(foreground=\"#442265\", font=(\"Arial\", 12 ))\n",
-    "\n",
-    "        res = chatbot_response(msg)\n",
-    "        ChatLog.insert(END, \"StudentBot: \" + res + '\\n\\n')\n",
-    "\n",
-    "        ChatLog.config(state=DISABLED)\n",
-    "        ChatLog.yview(END)\n",
-    "\n",
-    "\n",
-    "base = Tk()\n",
-    "base.title(\"StudentBot of the University of St.Gallen\")\n",
-    "base.config(bg=\"dark green\")\n",
-    "base.geometry(\"400x500\")\n",
-    "base.resizable(width=False, height=False)\n",
-    "icon_photo = PhotoImage(file = \"1000px-Universitaet-st-gallen.svg1.png\")\n",
-    "base.iconphoto(False, icon_photo)\n",
-    "\n",
-    "#Create a background using an image\n",
-    "send_button = PhotoImage(file = \"send_image.png\")\n",
-    "img_bg = Label(image=send_button)\n",
-    "img_bg.pack(pady=30)\n",
-    "\n",
-    "#Create Chat window\n",
-    "ChatLog = Text(base, bd=0, bg=\"white\", height=\"8\", width=\"50\", font=\"Arial\",)\n",
-    "\n",
-    "ChatLog.config(state=DISABLED)\n",
-    "\n",
-    "#Bind scrollbar to Chat window\n",
-    "scrollbar = Scrollbar(base, command=ChatLog.yview, cursor=\"\")\n",
-    "ChatLog['yscrollcommand'] = scrollbar.set\n",
-    "\n",
-    "#Create Button to send message\n",
-    "SendButton = Button(base, font=(\"Arial\",12,'bold'), text=\"Send\", width=\"12\", height=5,\n",
-    "                    bd=0, bg=\"#32de97\", activebackground=\"#3c9d9b\",fg='#ffffff',\n",
-    "                    command= send )\n",
-    "\n",
-    "#Create the box to enter message\n",
-    "EntryBox = Text(base, bd=0, bg=\"white\",width=\"29\", height=\"5\", font=\"Arial\")\n",
-    "#EntryBox.bind(\"<Return>\", send)\n",
-    "\n",
-    "\n",
-    "#Place all components on the screen\n",
-    "scrollbar.place(x=376,y=6, height=386)\n",
-    "ChatLog.place(x=6,y=6, height=386, width=370)\n",
-    "EntryBox.place(x=128, y=400, height=90, width=265)\n",
-    "SendButton.place(x=6, y=400, height=90)\n",
-    "\n",
-    "base.mainloop()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "infrared-preference",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "arctic-allen",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+#Creating GUI with tkinter
+import tkinter
+from tkinter import *
+
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Arial", 12 ))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "StudentBot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+
+ase = Tk()
+base.title("StudentBot of the University of St.Gallen")
+base.config(bg="dark green")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+icon_photo = PhotoImage(file = "1000px-Universitaet-st-gallen.svg1.png")
+base.iconphoto(False, icon_photo)
+
+#Create a background using an image
+send_button = PhotoImage(file = "send_image.png")
+img_bg = Label(image=send_button)
+img_bg.pack(pady=30)
+
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(base, font=("Arial",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=400, height=90, width=265)
+SendButton.place(x=6, y=400, height=90)
+
+base.mainloop()
